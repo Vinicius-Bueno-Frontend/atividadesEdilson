1515fcd6ee0554b19eadcd54238b2e68978df3a0
ViniciusGenthix <60518219+ViniciusGenthix@users.noreply.github.com>
2020-04-14 14:32:58 -0300

    Script finalizado

--- Aula03/AtividadeEstacionamento/scriptEstacionamento.js
+++ Aula03/AtividadeEstacionamento/scriptEstacionamento.js
@@ -1,1 +1,17 @@
+// ATIVIDADE (AULA03): ESTACIONAMENTO //
 
+function cobranca() {
+  var veiculo = prompt("Digite o seu veiculo: carro, ônibus ou caminhão");
+
+  var tempo = prompt(
+    "Digite quanto tempo seu veiculo ficou estacionado. Dê o valor arredondado em horas"
+  );
+
+  if (veiculo == "carro") {
+    alert("Você deve pagar: R$" + tempo * 2);
+  } else if (veiculo == "onibus") {
+    alert("Você deve pagar: R$" + tempo * 3);
+  } else {
+    alert("Você deve pagar: R$" + tempo * 4);
+  }
+}
